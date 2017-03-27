84bcae936c447d95ded9a2484d2873cc816f340f
sergeyr <snripa@gmail.com>
2017-03-27 21:00:35 +0300

    added 2 side binding, started with list binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+	id: number;
+	name: string;
+}
+
+const HEROES: Hero[] = [
+	{id:11, name:'Mr Good'},
+	{id:12, name:'Mr Bad'},
+	{id:13, name:'Mr Nice'},
+	{id:14, name:'Mr Awesome'},
+	{id:15, name:'Mr Evil'},
+	{id:16, name:'Mr Better'},
+	{id:17, name:'Mr Gorgeous'},
+	{id:18, name:'Mr Fantastic'},
+	{id:19, name:'Mr Pushy'},
+	{id:20, name:'Mr Daddy'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'yep!';
+  title = 'Tour of heros';
+  heroes = HEROES;
 }
+
